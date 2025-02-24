55862677d998fd446f3367f25ae91df9f6e2073b
salmoniscute <noralin249@gmail.com>
2025-02-25 01:00:08 +0800

    Add queue insertion functions
    Implemented the q_insert_head and q_insert_tail functions to insert
    elements into a queue. Initially used strcpy for string copying but
    replaced it with strncpy with explicit null termination to prevent
    vulnerabilities.
    
    Future work may include further enhancements.
    
    Change-Id: Idc9663425129dcd39eb14ba49af1430e8b774f6b

--- queue.c
+++ queue.c
@@ -13,7 +13,11 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
@@ -22,12 +26,42 @@ void q_free(struct list_head *head) {}
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+    element_t *new_element = malloc(sizeof(element_t));
+    if (!new_element)
+        return false;
+    size_t len = strlen(s) + 1;
+    new_element->value = malloc(len * sizeof(char));
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+    strncpy(new_element->value, s, len - 1);
+    new_element->value[len - 1] = '\0';
+    list_add(&new_element->list, head);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+    element_t *new_element = malloc(sizeof(element_t));
+    if (!new_element)
+        return false;
+    size_t len = strlen(s) + 1;
+    new_element->value = malloc(len * sizeof(char));
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+    strncpy(new_element->value, s, len - 1);
+    new_element->value[len - 1] = '\0';
+    list_add_tail(&new_element->list, head);
+
     return true;
 }
 
