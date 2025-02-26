e0a3d5c17ac21c86908ca7490ebb2dfcb1b6869d
Salmon Lin <noralin249@gmail.com>
2025-02-26 23:12:13 +0800

    Implement q_reverse function
    Add implementation of q_reverse() that reverses the order of all
    elements in the queue. The function operates by rearranging existing
    pointers without allocating or freeing any memory. It handles edge case
    with NULL or empty queues by having no effect.
    
    Change-Id: Ib68b1a65f0a71df4a433610679a30cda4853ce34

--- queue.c
+++ queue.c
@@ -149,7 +149,20 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+    struct list_head *curr = head;
+    struct list_head *tmp;
+
+    do {
+        tmp = curr->next;
+        curr->next = curr->prev;
+        curr->prev = tmp;
+        curr = tmp;
+    } while (curr != head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
